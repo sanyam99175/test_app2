4e3adf9ff813f474ddaf8215fcb17ff04fc4d6ac
Sanyam Arora <sanyam.arora@engagedly.com>
2022-09-23 15:07:51 +0530

    Add user id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
     belongs_to :user
+    validates :title, presence: true, length: {minimum: 5, maximum: 100}
+    validates :description, presence: true, length: {minimum: 8, maximum: 300}
 end
